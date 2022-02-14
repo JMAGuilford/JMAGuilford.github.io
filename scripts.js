9d74e53db60aace06989d92ae2b9d69c0aa2a359
JMAGuilford <jonathanguilford@hotmail.co.uk>
2022-02-13 23:06:09 -0500

    2021-02-13-Redesign

--- scripts.js
+++ scripts.js
@@ -15,31 +15,24 @@ window.addEventListener('resize', () => {
 var mobileMenuShowing = false;
 
 function arrowFunction() {
-    let deviceWidth = window.innerWidth;
+    if (!mobileMenuShowing) {
+        document.getElementById("downArrow").classList.add("is-x");
+        document.getElementById("arrowNav").classList.add("is-x");
+        document.getElementById("mobileMenu").classList.add("is-displayed");
 
-    if (deviceWidth > 888 || !document.getElementById("downArrow").classList.contains("is-shrunken-nav")) { 
-        document.querySelector("#boger").scrollIntoView({ behavior: 'smooth' });
-     } else {
-        if (!mobileMenuShowing) {
-            document.getElementById("downArrow").classList.add("is-x");
-            document.getElementById("arrowNav").classList.add("is-x");
-            document.getElementById("mobileMenu").classList.add("is-displayed");
-
-            if(document.getElementById("mobileMenu").classList.contains("fade-out-time")) {
-                document.getElementById("mobileMenu").classList.remove("fade-out-time");
-            }
-
-            mobileMenuShowing = true;
-        } else {
-            document.getElementById("downArrow").classList.remove("is-x");
-            document.getElementById("arrowNav").classList.remove("is-x");
-            document.getElementById("mobileMenu").classList.add("fade-out-time");
-
-            mobileMenuShowing = false;
+        if(document.getElementById("mobileMenu").classList.contains("fade-out-time")) {
+            document.getElementById("mobileMenu").classList.remove("fade-out-time");
         }
-    } 
-}
 
+        mobileMenuShowing = true;
+    } else {
+        document.getElementById("downArrow").classList.remove("is-x");
+        document.getElementById("arrowNav").classList.remove("is-x");
+        document.getElementById("mobileMenu").classList.add("fade-out-time");
+
+        mobileMenuShowing = false;
+    }
+}
 
 function mobileNavClicked() {
     document.getElementById("downArrow").classList.remove("is-x");
@@ -75,43 +68,26 @@ document.onreadystatechange = () => {
         document.documentElement.style.setProperty('--rem', `${rem}px`);
 
         let scrollable = document.getElementById("scrollMain");
-
-        function shrinkNav() {
-            document.getElementById("stickyWrapper").classList.add("is-shrunken-nav");
-            document.getElementById("topSpacer").classList.add("is-shrunken-nav");
-            document.getElementById("centerHero").classList.add("is-shrunken-nav");
-            document.getElementById("nameNav").classList.add("is-shrunken-nav");
-            document.getElementById("arrowNav").classList.add("is-shrunken-nav");
-            document.getElementById("downArrow").classList.add("is-shrunken-nav");
-            document.getElementById("mainNav").classList.add("is-shrunken-nav");
-            document.getElementById("breaker").classList.add("is-shrunken-nav");
-        }
+        let element = document.getElementById("stickyWrapper");
 
         if(scrollSwitches) {
-            if (scrollable.scrollTop > (realHeight * 0.1)) {
-                shrinkNav();
+            if (scrollable.scrollTop > (realHeight * 0.5)) {
+                if (element.classList.contains("hidden")) {
+                    element.classList.remove("hidden");
+                }
             }
             
             scrollable.onscroll = function() {
-                let element = document.getElementById("stickyWrapper");
-                if (scrollable.scrollTop > (realHeight * 0.01)) {
-                    if (!element.classList.contains("is-shrunken-nav")) {
-                        console.log("FIRE!");
-                        shrinkNav();
+                if (scrollable.scrollTop > (realHeight * 0.5)) {
+                    if (element.classList.contains("hidden")) {
+                        element.classList.remove("hidden");
                     }
                 } else {
-                    if (element.classList.contains("is-shrunken-nav")) {
-                        document.getElementById("stickyWrapper").classList.remove("is-shrunken-nav");
-                        document.getElementById("topSpacer").classList.remove("is-shrunken-nav");
-                        document.getElementById("centerHero").classList.remove("is-shrunken-nav");
-                        document.getElementById("nameNav").classList.remove("is-shrunken-nav");
-                        document.getElementById("arrowNav").classList.remove("is-shrunken-nav");
-                        document.getElementById("downArrow").classList.remove("is-shrunken-nav");
-                        document.getElementById("mainNav").classList.remove("is-shrunken-nav");
-                        document.getElementById("breaker").classList.remove("is-shrunken-nav");
+                    if (!element.classList.contains("hidden")) {
+                        element.classList.add("hidden");
                     }
                 }
             }
-        }
+        } 
     }
 };
